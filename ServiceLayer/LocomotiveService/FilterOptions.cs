76b4ca3d6c461132ef6583a3ade028e8f1c8b9a8
Simon Andreassen <simo531i@elevcampus.dk>
2021-10-14 11:14:36 +0200

    Added filter, paging, and search

---
+++ ServiceLayer/LocomotiveService/FilterOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceLayer.LocomotiveService
+{
+    public class FilterOptions
+    {
+        public ICollection<string> Tags { get; set; }
+    }
+}
--- ServiceLayer/LocomotiveService/QueryOptions.cs
+++ ServiceLayer/LocomotiveService/QueryOptions.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ServiceLayer.LocomotiveService
 {
     public enum OrderByOptions
     {
+        [Display(Name = "Name (Ascending)")]
         ByNameAsc = 0,
+        [Display(Name = "Name (Descending)")]
         ByNameDesc,
+        [Display(Name = "Price (Ascending)")]
         ByPriceAsc,
+        [Display(Name = "Price (Descending)")]
         ByPriceDesc,
+        [Display(Name = "Railway company (Ascending)")]
         ByRailwayCompanyAsc,
+        [Display(Name = "Railway company (Descending)")]
         ByRailwayCompanyDesc
     }
 
     public class QueryOptions
     {
         public OrderByOptions OrderByOptions { get; set; }
+        public FilterOptions FilterOptions { get; set; }
     }
 }
