1aab3a31662571def67407e0de0d73fde108717c
Simon Andreassen <simo531i@elevcampus.dk>
2021-10-18 14:02:56 +0200

    Added generic cookie banner

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -27,6 +27,12 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = Microsoft.AspNetCore.Http.SameSiteMode.Lax;
+            });
+            
             services.AddDbContext<EShopContext>(options => options = new DbContextOptionsBuilder());
 
             services.AddScoped<ILocomotiveService, LocomotiveService>();
@@ -51,6 +57,7 @@ namespace WebApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
